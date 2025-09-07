4317186b55a66b069f157f0f888e5e0e9ba8a303
HelenLloydJeng <helenlloyd21@icloud.com>
2025-09-07 10:08:44 +0100

    fix(urls): correct route patterns/namespaces

--- scenarios/urls.py
+++ scenarios/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'scenarios'
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.ScenarioDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.ScenarioUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.ScenarioDeleteView.as_view(), name='delete'),
+    path('training/', include('training.urls')),
+
 ]
